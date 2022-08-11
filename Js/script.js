89d77131a8723e121d80dee93fbe68ec640c08cc
Aayush <aayushranjan200231@gmail.com>
2022-08-11 12:56:09 +0530

    first commit

---
+++ Js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["","Computer Science and Engineer","Software Engineer","Web Developer","Web Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
